533690e0b1b940fac37d10dce527de6a2a3ed1ae
Dmytro Vakulovych <dvakulov@softserveinc.com>
2015-06-15 14:19:46 +0300

    Cloudstack: change cloudstack providert layout

---
+++ terraform/cloudstack/hosts/main.tf
@@ -0,0 +1,38 @@
+# input variables
+variable api_url { }
+variable api_key { }
+variable secret_key { }
+variable network_id {default = "10.0.0.0/16"}
+variable template { default = "centos-7"}
+variable control_count { default = "3"}
+variable worker_count { default = "1"}
+variable control_type { default = "small" }
+variable worker_type { default = "small" }
+variable zone { default = "dc1" }
+variable short_name { default = "mi" }
+variable control_name { }
+variable worker_name { }
+
+provider "cloudstack" {
+  api_url   = "${ var.api_url }"
+  api_key    = "${ var.api_key }"
+  secret_key = "${ var.secret_key }"
+}
+
+resource "cloudstack_instance" "mi-control-nodes" {
+  zone = "${ var.zone }"
+  service_offering = "${ var.control_type }"
+  template = "${ var.template }"
+  control_name = "${ var.short_name }-control-${ format("%02d", count.index+1) }"
+  network = "${ var.network_id }"
+  count = "${ var.control_count }"
+}
+
+resource "cloudstack_instance" "mi-worker-nodes" {
+  zone = "${ var.zone }"
+  service_offering = "${ var.worker_type }"
+  template = "${ var.template }"
+  worker_name = "${ var.short_name }-worker-${ format("%02d", count.index+1) }"
+  network = "${ var.network_id }"
+  count = "${ var.worker_count }"
+}
---
+++ terraform/cloudstack/keypair/main.tf
@@ -0,0 +1,23 @@
+# input variables
+variable api_url { }
+variable api_key { }
+variable secret_key { }
+variable short_name { default = "mi" }
+variable public_key_filename { default = "~/.ssh/id_rsa.pub" }
+
+provider "cloudstack" {
+  api_url   = "${ var.api_url }"
+  api_key    = "${ var.api_key }"
+  secret_key = "${ var.secret_key }"
+}
+
+# create resources
+resource "cloudstack_ssh_keypair" "default" {
+  name = "${var.short_name}-key"
+  public_key = "${file(var.public_key_filename)}"
+}
+
+# output variables
+output "keypair_name" {
+  value = "${cloudstack_ssh_keypair.default.id}"
+}
