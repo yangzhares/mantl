660d073087b061bd07739ba1845504a20eccaba0
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-01-12 09:49:19 -0600

    main playbook now coupled with wait-for-hosts

--- testing/integration-test.sh
+++ testing/integration-test.sh
@@ -11,6 +11,7 @@ RETRY_ATTEMPTS=3
 function retry_command() {
 	for i in `seq 1 $RETRY_ATTEMPTS`
 	do
+		echo -e "CMD: $1\tTRY: $i"
 		eval $1
 		RETRY=$?
 		if [ $RETRY -eq 0 ]
@@ -24,12 +25,14 @@ function retry_command() {
 	fi
 }
 
+./security-setup
+
 terraform get
 retry_command "terraform apply -state=$TERRAFORM_STATE_ROOT/terraform.tfstate"
 
-
-ansible-playbook playbooks/wait-for-hosts.yml --private-key ~/.ssh/id_rsa
-retry_command "ansible-playbook terraform.yml --extra-vars=@security.yml --private-key ~/.ssh/id_rsa"
+ansible_commands="ansible-playbook playbooks/wait-for-hosts.yml --private-key ~/.ssh/id_rsa"
+ansible_commands+=" && ansible-playbook terraform.yml --extra-vars=@security.yml --private-key ~/.ssh/id_rsa"
+retry_command "$ansible_commands"
 
 control_hosts=$(plugins/inventory/terraform.py --hostfile | awk '/control/ {print $1}')
 
